1077c6454af7d46ebe72f118e0dcca99ef52e125
Hans Naert <hans.naert@vives.be>
2024-10-22 12:12:34 +0200

    change "then" to "await" (Promise => Async Await)

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const i2c=require('i2c-bus');
+const i2c = require('i2c-bus');
 
-const TC74_ADDR=0x48;
-const TEMP_REG=0x00;
-
-i2c.openPromisified(1).
-then(i2c1=>i2c1.readByte(TC74_ADDR,TEMP_REG).
-then(rawData=>console.log(rawData)).
-then(_=>i2c1.close())
-).catch(console.log);
+const TC74_ADDR = 0x48;
+const TEMP_REG = 0x00;
+(async () => {
+    try {
+        let i2c1 = await i2c.openPromisified(1);
+        let i2c1_byte = await i2c1.readByte(TC74_ADDR, TEMP_REG);
+        let rawData = await i2c1_byte;
+        console.log(rawData);
+        i2c1.close()
+    }
+    catch (error) { console.log(error) }
+})();
